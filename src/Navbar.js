e09fe07bb36b0570463e027a3583fe575db9c309
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-13 12:22:41 +0100

    Fin exo3

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+
+function Nav() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>Portfolio</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
