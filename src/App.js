7acf00a7baf7197096d1638b6620f0d062f4c5c3
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-13 12:18:08 +0100

    Modification du component App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import './App.css';
+import Kappa from './img/Kappa.png';
 
 function App() {
   return (
-    
+    <div>
+      <h1>Exercice 3</h1>
+      <img src={Kappa}></img>
+    </div>
   );
 }
 
